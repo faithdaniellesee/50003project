1f8db2ee4632c384599e4602a115dc9c11cb14ba
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-02-25 15:46:46 +0800

    Updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,18 +11,21 @@ def index():
 shared_var = None
 req_counter = 0
 
-bearer_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODI3NzAwNTEs' \
-               'ImlkIjoid2VpamluX3RhbkBteW1haWwuc3V0ZC5lZHUuc2ciLCJvcmlnX2lhdCI6MTU1' \
-               'MDYyOTI1MX0.jQb5CD8vRw7rU5StmJEE9q0-pwMQStTJ8vl8ZE6pgvI'
+bearer_token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODI3NzAwNTEs" \
+               "ImlkIjoid2VpamluX3RhbkBteW1haWwuc3V0ZC5lZHUuc2ciLCJvcmlnX2lhdCI6MTU1" \
+               "MDYyOTI1MX0.jQb5CD8vRw7rU5StmJEE9q0-pwMQStTJ8vl8ZE6pgvI"
 
 
 @app.route('/api', methods=['GET'])
 def apicall():
-    url = 'https://ug-api.acnapiv3.io/swivel/language-identification/lang-2.0?of=json&txt='
+    url = 'https://ug-api.acnapiv3.io/swivel/language-identification/lang-2.0?'
     text = 'Hi my name is'
-    url = url + text
+    url = "https://ug-api.acnapiv3.io/swivel/language-identification/lang-2.0?of=json&txt=hello what is up"
+    data = {"txt": 'Hi my name is'}
 
-    headers = {"Authorization": bearer_token}
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODI3NzAwNTEs"
+                                + "ImlkIjoid2VpamluX3RhbkBteW1haWwuc3V0ZC5lZHUuc2ciLCJvcmlnX2lhdCI6MTU1"
+                                + "MDYyOTI1MX0.jQb5CD8vRw7rU5StmJEE9q0-pwMQStTJ8vl8ZE6pgvI"}
     response = requests.get(url, headers=headers)
     value = response.text
 
