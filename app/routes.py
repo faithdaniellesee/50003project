70ff9d154db07dca8434e20ae898ba43e3126de1
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-02-28 19:19:50 +0800

    Updated login page

--- app/routes.py
+++ app/routes.py
@@ -45,6 +45,7 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
   
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
