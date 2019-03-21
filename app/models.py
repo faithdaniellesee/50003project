15327b953ea16dfb9d82857ad1955e5b0ae14262
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-03-21 23:53:13 +0800

    Updated

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -22,4 +21,3 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
