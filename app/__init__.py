85a30b05a6cdbbbe76b7c5c7a3258a21a379f59f
Faith <faith_see@mymail.sutd.edu.sg>
2019-03-22 01:00:34 +0800

    Created admin branch

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-# not needed
-# csrf = CSRFProtect(app)
-# csrf.init_app(app)
-app.secret_key = 'myverylongsecretkey'
 
+from app.models import User
+# For flask admin
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
 
 from app import routes, models
