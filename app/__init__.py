ad3be95439f7f627172ba3f8b43e02ae0da75875
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-04-08 19:50:16 +0800

    Update api

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user
