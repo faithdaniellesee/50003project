94525e45541e6549af71dc3260bb351d7ff97a19
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-04-11 19:46:43 +0800

    Fixed login page

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
      #   'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SECURITY_SEND_REGISTER_EMAIL = False
     SECURITY_UNAUTHORIZED_VIEW = '/login'
     USER_ENABLE_EMAIL = False
