861c104fecbc44ecddcce21eceba9b782c34ccd0
BillioJeverson <billio_jeverson@mymail.sutd.edu.sg>
2020-01-06 18:10:33 +0800

    change to local for the sake of demo

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI =  'mysql://root:'+ 'root' + '@localhost:3307/accenture'
+    SQLALCHEMY_DATABASE_URI =  'mysql://root:'+ 'RootBillio' + '@localhost:3306/accenture'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
      #   'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
